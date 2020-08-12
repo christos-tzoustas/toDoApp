2edc793891ce9010ad3ff0aafa1ac222d2994411
christos-tzoustas <ctzoustas@hotmail.com>
2020-08-12 18:51:44 +0200

    Updates

---
+++ precache-manifest.26fe68011a350b75e30b57f3bfc4ee6e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18ab55fc9e69a59a64f17024ac852544",
+    "url": "/toDoApp/index.html"
+  },
+  {
+    "revision": "8cd3aa444a085f3f460e",
+    "url": "/toDoApp/static/css/main.56c1e297.chunk.css"
+  },
+  {
+    "revision": "c03ad8f9425fc4afa018",
+    "url": "/toDoApp/static/js/2.6e80fa7c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/toDoApp/static/js/2.6e80fa7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8cd3aa444a085f3f460e",
+    "url": "/toDoApp/static/js/main.fc1a3cae.chunk.js"
+  },
+  {
+    "revision": "e7a2e86ca7666d5eb780",
+    "url": "/toDoApp/static/js/runtime-main.dec9a699.js"
+  }
+]);
